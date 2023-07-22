2aba44b78a765fdb7cb8b2bc4a8e7b8136a4c8c8
Sasukexo <aswinash1002@gmail.com>
2023-07-22 22:52:16 +0530

    created models for hacker and organiser

--- hackportal_dj/login/models.py
+++ hackportal_dj/login/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# Create 
+class Hacker(models.Model):
+    lead_name = models.CharField(max_length=100)
+    lead_email = models.EmailField()
+    member1_name = models.CharField(max_length=100)
+    member2_name = models.CharField(max_length=100)
+    member3_name = models.CharField(max_length=100)
+    member4_name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    
+
+class Organizer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    
+    # Add other fields as needed, such as organization, role, etc.
+
+    def _str_(self):
+        return self.name
--- hackportal_dj/organiser/models.py
+++ hackportal_dj/organiser/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.from django.db import models
+
+
 
 class Hackathon(models.Model):
     id = models.AutoField(primary_key=True)
