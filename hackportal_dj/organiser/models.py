f11ea7016283f47c5da9f8253b6bf92ca65bc289
aviiciii <laavesh1@gmail.com>
2023-07-22 23:12:48 +0530

    sponsor db changes

--- hackportal_dj/organiser/models.py
+++ hackportal_dj/organiser/models.py
@@ -6,9 +6,9 @@ from django.db import models
 
 class Sponsor(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
-    website = models.URLField()
-    description = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    description = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.name
