76243859f64e6da2d8caededc8b55107ba121399
aviiciii <laavesh1@gmail.com>
2023-07-22 22:25:43 +0530

    hackathon details done

--- hackportal_dj/organiser/models.py
+++ hackportal_dj/organiser/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Hackathon(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
+    duration = models.CharField(max_length=200, blank=True, null=True)
+    theme = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    eligibility = models.CharField(max_length=200, blank=True, null=True)    
+    team_size = models.CharField(max_length=20, blank=True, null=True)
+    prizes = models.CharField(max_length=200, blank=True, null=True)
+    sponsors = models.ManyToManyField('Sponsor', blank=True)
+    # Add other fields as needed.
+
+    def __str__(self):
+        return self.name
+
+
+
+class Sponsor(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    website = models.URLField()
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
--- hackportal_dj/organiser/urls.py
+++ hackportal_dj/organiser/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="o_index"),
-    
+    path("hackthon/", views.hackathon_details, name="o_hackathon_details"),
 ]
