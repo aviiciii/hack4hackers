7d091a9d087ee105b7d1adf56160ff8a7d034d7a
Sasukexo <aswinash1002@gmail.com>
2023-07-22 23:55:20 +0530

    added layout page and index page for hackers

--- hackportal_dj/hacker/views.py
+++ hackportal_dj/hacker/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+#i
+from .models import Sponsor
+from .models import Hackathon
 # Create your views here.
+
+def index(request):
+    return render(request, 'hacker/index.html')
+
+def hackathon_details(request):
+    return render(request, 'hacker/hackathon_details.html')
+
+def sponsor_details(request):
+    return render(request, 'hacker/sponsor_details.html')
+
+def team_details(request):
+    return render(request, 'hacker/team_details.html')
