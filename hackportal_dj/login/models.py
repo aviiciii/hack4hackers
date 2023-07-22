c27bf7a277a21c9a555b5e104dc0bd56215423e4
aviiciii <laavesh1@gmail.com>
2023-07-22 22:56:39 +0530

    models for login init

--- hackportal_dj/login/models.py
+++ hackportal_dj/login/models.py
@@ -2,23 +2,22 @@ from django.db import models
 
 # Create 
 class Hacker(models.Model):
-    team_name = models.CharField(max_length=100)
-    lead_name = models.CharField(max_length=100)
-    lead_email = models.EmailField()
-    member1_name = models.CharField(max_length=100)
-    member2_name = models.CharField(max_length=100)
-    member3_name = models.CharField(max_length=100)
-    member4_name = models.CharField(max_length=100)
+    team_name = models.CharField(max_length=100, null=True, blank=True)
+    lead_name = models.CharField(max_length=100, null=True, blank=True)
+    lead_email = models.EmailField(null=True, blank=True)
+    member1_name = models.CharField(max_length=100, null=True, blank=True)
+    member2_name = models.CharField(max_length=100, null=True, blank=True)
+    member3_name = models.CharField(max_length=100, null=True, blank=True)
+    member4_name = models.CharField(max_length=100, null=True, blank=True)
+    
     def __str__(self):
         return self.name
     
 
-class Organizer(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-    
-    # Add other fields as needed, such as organization, role, etc.
+class Organiser(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    password = models.CharField(max_length=100, null=True, blank=True)
 
     def _str_(self):
         return self.name
