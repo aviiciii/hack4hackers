fef2d1c949b65eba005fd7d91f8571916a1f6ba5
aviiciii <laavesh1@gmail.com>
2023-07-23 13:31:00 +0530

    added organiser model

--- hackportal_dj/organiser/urls.py
+++ hackportal_dj/organiser/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.index, name="o_index"),
     path("hackthon/", views.hackathon_details, name="o_hackathon_details"),
     path("sponsor/", views.sponsor_details, name="o_sponsor_details"),
+    path("announcement/", views.announcement, name="o_announcement"),
 ]
