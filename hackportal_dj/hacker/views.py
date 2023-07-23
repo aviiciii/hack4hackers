42de7a927e3638ee3ba45e7b92b6d4d074ca8df7
aviiciii <laavesh1@gmail.com>
2023-07-23 14:23:52 +0530

    hacker dash landing team details added

--- hackportal_dj/hacker/views.py
+++ hackportal_dj/hacker/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
-#i
-#from .models import Sponsor
-#from .models import Hackathon
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+# messages
+from django.contrib import messages
+
+from login.models import Hacker, Organiser
+from organiser.models import Announcement, Sponsor, Hackathon
 
 def index(request):
+    user = request.user
+    try:
+        hacker = Hacker.objects.get(team_name=user)
+    except:
+        messages.error(request, 'You are not a hacker. Login as hacker!')
+        return redirect('login')
+    if user.is_authenticated and hacker:
+        announcements = Announcement.objects.all()
+        sponsors = Sponsor.objects.all()
+        hackathons = Hackathon.objects.all()
+        context = {
+            'hacker': hacker,
+            'announcements': announcements,
+            'sponsors': sponsors,
+            'hackathons': hackathons,
+        }
+        return render(request, 'hacker/index.html', context)
+        
+
     return render(request, 'hacker/index.html')
 
 
